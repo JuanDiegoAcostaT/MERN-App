f652bfdb92462ca1ad908685ecd178e1e11ef74e
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-05-07 17:49:40 -0500

    nodemon

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 //Settings
@@ -7,12 +8,15 @@ app.set('PORT', process.env.PORT || 3000);
 
 
 //Middlewares
-
-
-app.use(express.urlencoded({ extended : false }));
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
 
 
+// Routes
+
+app.get('/api/users', (req, res, next) => {  res.send('Users Routes')})
+app.get('/api/notes', (req, res, next) => {  res.send('Notes Routes')})
 
 //Export
 
