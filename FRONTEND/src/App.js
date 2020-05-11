2ecb00eb37076c560c54476a3056c7ea9d9f64f7
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-05-11 18:05:50 -0500

    Axios and Create User Component

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -13,13 +13,15 @@ const App = () => {
   return (
     <>
       <Navigation />
-      <Router>
-        <NotFound default />
-        <NotesList exact path='/' />
-        <CreateNote exact path='/edit/:id' />
-        <CreateNote exact path='/create' />
-        <CreateUser exact path='/user' />
-      </Router>
+      <div className='container p-4'>
+        <Router>
+          <NotFound default />
+          <NotesList exact path='/' />
+          <CreateNote exact path='/edit/:id' />
+          <CreateNote exact path='/create' />
+          <CreateUser exact path='/user' />
+        </Router>
+      </div>
     </>
   );
 };
--- FRONTEND/src/components/CreateUser.js
+++ FRONTEND/src/components/CreateUser.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CreateUser() {
+
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const fecthData = async () => {
+      const res = await axios.get('http://localhost:3000/api/users');
+      setUsers(res.data);
+    };
+    fecthData();
+  }, [users]);
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+    console.log(username);
+  };
+
   return (
-    <div>
-      <h1>Create User</h1>
+    <div className='row'>
+      <div className='col-md-4'>
+        <div className='card card-body'>
+          <h3>Create New User</h3>
+          <form action=''>
+            <div className='form-group'>
+              <input
+                type='text'
+                className='form-control'
+                onChange={onChangeUsername}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className='col-md-8'>
+        <ul className='list-group'>
+          {
+            users.map((user) => (
+              <li key={user._id} className='list-group-item list-group-item-action'>
+                {
+                  user.username
+                }
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
